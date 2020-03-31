248c3a9e2ccbf612d35cef1314fdce3a5b92687b
Gabriel Neri <neri.gabriel78@gmail.com>
2020-03-31 15:12:41 -0300

    add form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public frmLogin: any = {
+    email: '',
+    senha: ''
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public login(): void {
+    alert(this.frmLogin.email+ " | "+ this.frmLogin.senha);
   }
 
 }
